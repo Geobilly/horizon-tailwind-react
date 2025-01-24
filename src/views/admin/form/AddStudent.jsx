e514dc7e5f196322cba444288b038d9131373aa6
George Abban <102395479+Geobilly@users.noreply.github.com>
2025-01-24 09:17:06 +0000

    cc
    ccc

--- src/views/admin/form/AddStudent.jsx
+++ src/views/admin/form/AddStudent.jsx
@@ -55,7 +55,7 @@ const AddStudent = ({ isOpen, onClose }) => {
     };
 
     try {
-      const response = await axios.post("http://127.0.0.1:5000/addstudent", studentData);
+      const response = await axios.post("https://edupayapi.kempshotsportsacademy.com/addstudent", studentData);
       alert(`Success: ${response.data.message}, Student ID: ${response.data.student_id}`);
       onClose(); // Close the modal after the success alert
     } catch (error) {
